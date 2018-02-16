7fce8b825e34549eea091c094e87c5cab847162e
matth3wology <matthewlsneed@icloud.com>
2018-02-16 11:17:36 -0800

    Addint FFT & WAV Plot

---
+++ fft_plot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Need to get (x,y) coordinates of a DFFT from a wave sample. @matth3wology
+
+#Import wav file to matplot Frequency Domain
+m = wave.open("1.wav","r")
+
+#Convert wav to numpy matrix
+frames = m.readframes(-1)
+frames = np.fromstring(frames,"Int16")
+
+channels = [[] for channel in range(m.getnchannels())]
+
+#Checking the number of channels to take both Mono and Stereo
+for a,b in enumerate(frames):
+    channels[a%len(channels)].append(b)
+
+signal = channels[0]
+
+N = len(signal)
+fft = np.fft.fft(signal)
+
+y = 2.0/N * np.abs(fft[:N//2])
+x = np.linspace(0.0,m.getframerate()/2.0,N/2)
+plt.plot(x,y)
+
+#Show the plot
+plt.show()
---
+++ wav_plot.py
@@ -0,0 +1,23 @@
+#!/bin/python
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = wave.open('1.wav','r')
+frames = m.readframes(-1)
+frames = np.fromstring(frames,"Int16")
+
+channels = [[] for channel in range(2)]
+
+for a,b in enumerate(frames):
+	channels[a%2].append(b)
+
+signal = channels[0]
+time = len(signal)/44100
+x=np.linspace(0,time,len(signal))
+
+plt.plot(x,signal)
+plt.show()
+
+
+
